da0fb34c97800a0a043870fdec758c241455c6ce
Matthew Hall <matthew.hall@threatstream.com>
2015-12-14 21:59:13 -0800

    ack-1.96.rb: add ack 1.96 to homebrew-versions

---
+++ ack-1.96.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Ack196 < Formula
+  homepage 'http://beyondgrep.com/'
+  url 'https://github.com/petdance/ack/archive/1.96.tar.gz'
+  sha1 '5d53b2b6f285e222a8459730495d6b07c692edd6'
+
+  def install
+    system 'pod2man', 'ack', 'ack.1'
+    man1.install 'ack.1'
+    bin.install 'ack'
+    bash_completion.install 'etc/ack.bash_completion.sh'
+    zsh_completion.install 'etc/ack.zsh_completion' => '_ack'
+  end
+
+  test do
+    system "#{bin}/ack", 'brew', '/usr/share/dict/words'
+  end
+end
+
